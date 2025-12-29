ca76c02abd46596288adbe3eaf80abba0b836ece
Amrou <amrou200799@gmail.com>
2025-12-29 08:35:01 +0400

    Created CBEM v1.5 and will implement Later

---
+++ CBEMv1.5_LMR/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using static MoveFlag;
+using static MoveGenerator;
+
+class Program
+{
+    public const string EmptyBoard = "8/8/8/8/8/8/8/8 w - - ";
+    public const string StartPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    public const string TrickyPosition = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+    public const string PinPosition = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+    public const string Position5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+
+    static void Main()
+    {
+        PieceAttacks.InitAll();
+
+        // Debug mode variable
+        bool debug = true;
+
+        if (debug)
+        {
+            // Parse FEN and print board
+            Board.ParseFEN(TrickyPosition);
+
+            // Placeholder for evaluation when you implement it
+            Console.WriteLine($"score: {Evaluation.Evaluate()}");
+
+            Search.SearchPosition(6);
+        }
+        else
+        {
+            // Connect to the GUI
+            Uci.UciLoop();
+        }
+    }
+
+    static long nodes;
+    public static int GetTimeMs() => Environment.TickCount;
+
+    static void PerftDriver(int depth)
+    {
+        if (depth == 0)
+        {
+            nodes++;
+            return;
+        }
+
+        MoveList moveList = new();
+        GenerateMoves(ref moveList);
+
+        for (int i = 0; i < moveList.count; i++)
+        {
+            BoardState state = Board.CopyBoard();
+
+            if (MakeMove(moveList.moves[i], (int)allMoves) == 0)
+            {
+                continue;
+            }
+
+            PerftDriver(depth - 1);
+
+            Board.TakeBack(state);
+        }
+    }
+
+    static void PerftTest(int depth)
+    {
+        Console.WriteLine("\n     Performance test\n");
+
+        MoveList moveList = new();
+        GenerateMoves(ref moveList);
+
+        int start = GetTimeMs();
+
+        for (int i = 0; i < moveList.count; i++)
+        {
+            BoardState state = Board.CopyBoard();
+
+            if (MakeMove(moveList.moves[i], (int)allMoves) == 0)
+                continue;
+
+            long cumulative = nodes;
+
+            PerftDriver(depth - 1);
+
+            long oldNodes = nodes - cumulative;
+
+            Board.TakeBack(state);
+
+            Console.WriteLine($"     move: {MoveEncoding.GetMove(moveList.moves[i])}  nodes: {oldNodes}");
+        }
+
+        Console.WriteLine($"\n    Depth: {depth}");
+        Console.WriteLine($"    Nodes: {nodes}");
+        Console.WriteLine($"     Time: {GetTimeMs() - start}ms\n");
+    }
+}
---
+++ CBEMv1.5_LMR/Search.cs
@@ -0,0 +1,369 @@
+using System.Drawing;
+using static Board;
+using static MoveEncoding;
+using static MoveGenerator;
+
+public static class Search
+{
+    // half move counter
+    private static int ply;
+    // nodes counter
+    private static long nodes;
+    // MAX PLY to reach within the search (to prevent overflow)
+    private static readonly int maxPly = 64;
+
+    // MVV LVA [attacker][victim]
+    private static readonly int[,] mvv_lva = new int[,]
+    {
+        {105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605},
+        {104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604},
+        {103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603},
+        {102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602},
+        {101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601},
+        {100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600},
+
+        {105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605},
+        {104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604},
+        {103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603},
+        {102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602},
+        {101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601},
+        {100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600}
+    };
+
+    static readonly int[,] killerMoves = new int[2, maxPly];
+    // history moves [piece][SQUARE] (not ply)
+    static readonly int[,] historyMoves = new int[12, 64];
+
+    static readonly int[,] pvTable = new int[maxPly, maxPly];
+
+    static readonly int[] pvLength = new int[maxPly];
+
+    static bool followpv = false, scorepv = false;
+
+
+    // Main search routine using negamax with alpha-beta pruning
+    public static void SearchPosition(int depth)
+    {
+        nodes = 0;
+        followpv = false;
+        scorepv = false;
+
+        // clear helper data structures
+        Array.Clear(pvTable);
+        Array.Clear(pvLength);
+        Array.Clear(killerMoves);
+        Array.Clear(historyMoves);
+
+        // iteretive deepening
+        for (int currentDepth = 1; currentDepth <= depth; currentDepth++)
+        {
+            nodes = 0;
+            followpv = true;
+
+            // find best move within a given position
+            int score = AlphaBeta(-50000, 50000, currentDepth);
+
+            Console.Write($"info score cp {score} depth {currentDepth} nodes {nodes} pv ");
+
+            // loop over the moves within a PV line
+            for (int count = 0; count < pvLength[0]; count++)
+            {
+                // print PV move
+                Console.Write($"{GetMove(pvTable[0, count])} ");
+            }
+
+            // print new line
+            Console.WriteLine();
+        }
+
+        // best move placeholder
+        Console.Write($"bestmove {GetMove(pvTable[0, 0])}");
+        Console.WriteLine();
+    }
+
+
+    // negamax alpha beta search
+    private static int AlphaBeta(int alpha, int beta, int depth)
+    {
+        bool foundPv = false;
+        // init PV length
+        pvLength[ply] = ply;
+
+        // recursion escape condition
+        if (depth == 0)
+            // return evaluation
+            return Quiescence(alpha, beta);
+
+        // we are too deep, hence there's an overflow on arrays relying on maxPly constant
+        if (ply > maxPly - 1)
+            return Evaluation.Evaluate();
+
+        // increment nodes count
+        nodes++;
+
+        bool inCheck = PieceAttacks.IsSquareAttacked((side == (int)Side.white) ? BitboardOperations.GetLs1bIndex(bitboards[K]) : BitboardOperations.GetLs1bIndex(bitboards[k]), side ^ 1);
+
+        if (inCheck) depth++;
+
+        int legalMoves = 0;
+
+        // create move list instance
+        MoveList moveList = new();
+
+        // generate moves
+        GenerateMoves(ref moveList);
+
+        if (followpv) EnablepvScoring(moveList);
+
+        // score moves once
+        for (int i = 0; i < moveList.count; i++)
+            moveList.scores[i] = ScoreMove(moveList.moves[i]);
+
+        // sort by cached scores (descending)
+        Array.Sort(moveList.scores, moveList.moves, 0, moveList.count);
+        Array.Reverse(moveList.moves, 0, moveList.count);
+        Array.Reverse(moveList.scores, 0, moveList.count);
+
+        // loop over moves within a movelist
+        for (int count = 0; count < moveList.count; count++)
+        {
+            // preserve board state
+            BoardState state = CopyBoard();
+
+            // make sure to make only legal moves
+            if (MakeMove(moveList.moves[count], (int)MoveFlag.allMoves) == 0)
+            {
+                // skip to next move
+                continue;
+            }
+
+            // increment ply
+            ply++;
+
+            legalMoves++;
+
+            int score;
+            if (foundPv)
+            {
+                score = -AlphaBeta(-alpha - 1, -alpha, depth - 1);
+                if (score > alpha && score < beta)
+                {
+                    // we are in a Principal Variation window - search with full window
+                    score = -AlphaBeta(-beta, -alpha, depth - 1);
+                }
+            }
+            else
+            {
+                score = -AlphaBeta(-beta, -alpha, depth - 1);
+            }
+
+            // decrement ply
+            ply--;
+
+            // take move back
+            Board.TakeBack(state);
+
+            // fail-hard beta cutoff
+            if (score >= beta)
+            {
+                // update killer moves
+                killerMoves[1, ply] = killerMoves[0, ply];
+                killerMoves[0, ply] = moveList.moves[count];
+
+                // node (move) fails high
+                return beta;
+            }
+
+            // found a better move
+            if (score > alpha)
+            {
+                historyMoves[GetMovePiece(moveList.moves[count]), GetMoveTarget(moveList.moves[count])] += depth;
+
+                // PV node (move)
+                alpha = score;
+                foundPv = true;
+
+                // write PV move
+                pvTable[ply, ply] = moveList.moves[count];
+
+                for (int nextPly = ply + 1; nextPly < pvLength[ply + 1]; nextPly++)
+                    pvTable[ply, nextPly] = pvTable[ply + 1, nextPly];
+
+                // adjust PV length to include the current move
+                pvLength[ply] = pvLength[ply + 1];
+            }
+        }
+
+        if (legalMoves == 0)
+        {
+            if (inCheck)
+            {
+                // return mating score (assuming closest distnce to mating position)
+                return -49000 + ply;
+            }
+            else return 0;  // stalemate
+        }
+
+        // node (move) fails low
+        return alpha;
+    }
+
+    public static int Quiescence(int alpha, int beta, int depth = 0)
+    {
+        // depth limit to prevent infinite recursion
+        if (depth > 1)
+            return Evaluation.Evaluate();
+
+        // increment nodes count
+        nodes++;
+
+        int evaluation = Evaluation.Evaluate();
+
+        // fail-hard beta cutoff
+        if (evaluation >= beta)
+            return beta;
+
+        // found a better move
+        if (evaluation > alpha)
+            alpha = evaluation;
+
+        // check for check evasion
+        bool inCheck = PieceAttacks.IsSquareAttacked((side == (int)Side.white) ?
+            BitboardOperations.GetLs1bIndex(bitboards[K]) :
+            BitboardOperations.GetLs1bIndex(bitboards[k]), side ^ 1);
+
+        // create move list instance
+        MoveList moveList = new MoveList();
+
+        // generate moves - all moves if in check, only captures otherwise
+        if (inCheck)
+        {
+            GenerateMoves(ref moveList);
+        }
+        else
+        {
+            GenerateCaptureMoves(ref moveList);
+        }
+
+        // score moves once
+        for (int i = 0; i < moveList.count; i++)
+            moveList.scores[i] = ScoreMove(moveList.moves[i]);
+
+        // sort by cached scores (descending)
+        Array.Sort(moveList.scores, moveList.moves, 0, moveList.count);
+        Array.Reverse(moveList.moves, 0, moveList.count);
+        Array.Reverse(moveList.scores, 0, moveList.count);
+
+        // loop over moves within a movelist
+        for (int count = 0; count < moveList.count; count++)
+        {
+            int move = moveList.moves[count];
+
+            // preserve board state
+            BoardState state = CopyBoard();
+
+            // make sure to make only legal moves
+            if (MakeMove(move, (int)MoveFlag.allMoves) == 0)
+                continue;
+
+            // increment ply
+            ply++;
+
+            // score current move
+            int score = -Quiescence(-beta, -alpha, depth + 1);
+
+            // decrement ply
+            ply--;
+
+            // take move back
+            Board.TakeBack(state);
+
+            // fail-hard beta cutoff
+            if (score >= beta)
+                return beta;
+
+            // found a better move
+            if (score > alpha)
+                alpha = score;
+        }
+
+        return alpha;
+    }
+
+    // score moves
+    private static int ScoreMove(int move)
+    {
+        if (scorepv)
+        {
+            if (pvTable[0, ply] == move)
+            {
+                scorepv = false;
+                // give pv the highest score to search it first
+                return 20000;
+            }
+        }
+        // score capture move
+        if (GetMoveCapture(move) != 0)
+        {
+            // init target piece
+            int target_piece = (int)Piece.P;
+
+            // pick up bitboard piece index ranges depending on side
+            int start_piece, end_piece;
+
+            // pick up side to move
+            if (side == (int)Side.white)
+            {
+                start_piece = (int)Piece.p;
+                end_piece = (int)Piece.k;
+            }
+            else
+            {
+                start_piece = (int)Piece.P;
+                end_piece = (int)Piece.K;
+            }
+
+            // loop over bitboards opposite to the current side to move
+            for (int bb_piece = start_piece; bb_piece <= end_piece; bb_piece++)
+            {
+                // if there's a piece on the target square
+                if (BitboardOperations.GetBit(bitboards[bb_piece], GetMoveTarget(move)))
+                {
+                    // remove it from corresponding bitboard
+                    target_piece = bb_piece;
+                    break;
+                }
+            }
+
+            // score move by MVV LVA lookup [source piece][target piece]
+            return mvv_lva[GetMovePiece(move), target_piece] + 10000;
+        }
+
+        // score quiet move
+        else
+        {
+            if (killerMoves[0, ply] == move)
+                return 9000;
+            else if (killerMoves[1, ply] == move)
+                return 8000;
+            else
+                return historyMoves[GetMovePiece(move), GetMoveTarget(move)];
+        }
+    }
+
+    static void EnablepvScoring(MoveList moveList)
+    {
+        // disable following PV
+        followpv = false;
+
+        for (int count = 0; count < moveList.count; count++)
+        {
+            // make sure we hit pv move
+            if (pvTable[0, ply] == moveList.moves[count])
+            {
+                scorepv = true;
+                followpv = true;
+            }
+        }
+    }
+}
